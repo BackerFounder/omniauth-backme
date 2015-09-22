cbf728a32f698f23d619ba4d301f8f5dfc1e4852
ronald.hsu <hothero0705@gmail.com>
2015-09-22 20:24:25 +0800

    update backme oauth options to https protocol

--- lib/omniauth/strategies/backme.rb
+++ lib/omniauth/strategies/backme.rb
@@ -8,7 +8,7 @@ module OmniAuth
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
       option :client_options, {
-        :site =>  "http://graph.backme.tw"
+        :site =>  "https://graph.backme.tw"
       }
  
       # Disable state-based CSRF protection
