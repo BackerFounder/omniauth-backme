c261c5656a07f25a7da5a92164828fa1f88a4743
ronald.hsu <hothero0705@gmail.com>
2015-11-30 22:07:43 +0800

    add omniauth-oauth2 dependency & try to fix callback_url problem

--- lib/omniauth/strategies/backme.rb
+++ lib/omniauth/strategies/backme.rb
@@ -37,6 +37,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= access_token.get("oauth/me.json").parsed
       end
+
+      def callback_url
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
